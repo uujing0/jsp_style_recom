66f7d27fcb059e1d9afeb431ff8765a8e2e68efd
Na il gang <nik1300@naver.com>
2019-10-31 10:51:49 +0900

    가입 폼 액션

---
+++ jsp_style_recom/src/service/JoinFormAction.java
@@ -0,0 +1,24 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class JoinFormAction implements CommandProcess {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return "joinForm.jsp";
+	}
+
+}
